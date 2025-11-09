95fe121347f20c77ead11bd740e52487b47ddec2
Dev-Abdelrhman <abdelrhmanfawzy38@gmail.com>
2025-11-09 21:37:13 +0200

    refactor: standardize import paths and remove unused middleware imports

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,15 +1,8 @@
-import {
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  RequestMethod,
-} from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { UsersModule } from 'src/users/users.module';
-import { JwtMiddleware } from '../auth/middleware/jwt.middleware';
-import { RoleMiddleware } from '../auth/middleware/role.middleware';
 import { AuthModule } from '../auth/auth.module';
 import { JwtService } from '@nestjs/jwt';
 
