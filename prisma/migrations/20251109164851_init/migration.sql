b574a834d8f97cca52be2d3adb4dd5c92db0cb2b
Dev-Abdelrhman <abdelrhmanfawzy38@gmail.com>
2025-11-11 05:54:19 +0200

    Add Comment table with hierarchical structure and user relations

--- prisma/migrations/20251109164851_init/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "Post" ADD COLUMN     "privacy" TEXT DEFAULT 'public',
-ALTER COLUMN "published" SET DEFAULT true;
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "isActive" BOOLEAN NOT NULL DEFAULT true;
