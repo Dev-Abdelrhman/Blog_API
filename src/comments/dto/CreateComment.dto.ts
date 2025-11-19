580f6f7ff76def4e27bd1de590eebf143572d9a5
Dev-Abdelrhman <abdelrhmanfawzy38@gmail.com>
2025-11-19 10:43:48 +0200

    Remove react field from CreateCommentDto

--- src/comments/dto/CreateComment.dto.ts
+++ src/comments/dto/CreateComment.dto.ts
@@ -1,13 +1,10 @@
-import { IsInt, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { IsEnum, IsInt, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
 
 export class CreateCommentDto {
   @IsNotEmpty()
   @IsString()
   content: string;
-
-  @IsOptional()
-  @IsString()
-  react?: string;
 
   @IsOptional()
   @IsInt()
