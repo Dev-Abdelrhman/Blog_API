71694f3f1f0085f69402314d7c2719a28894d21b
Dev-Abdelrhman <abdelrhmanfawzy38@gmail.com>
2025-11-19 10:43:30 +0200

    Remove role field from UpdateUserDto

--- src/users/dtos/UpdateUser.dto.ts
+++ src/users/dtos/UpdateUser.dto.ts
@@ -1,13 +1,10 @@
 import { IsEmail, IsOptional, IsEnum } from 'class-validator';
+
 export class UpdateUserDto {
   @IsEmail()
   @IsOptional()
   email?: string;
-
-  @IsEnum(['reader', 'author'])
-  @IsOptional()
-  role?: string;
-
+  
   @IsOptional()
   name?: string;
 }
