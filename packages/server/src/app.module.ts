c9af114fa8a3627c30d28ce426b93d01d1dc8989
Dev-Abdelrhman <abdelrhmanfawzy38@gmail.com>
2025-12-23 04:56:55 +0200

    Add .js file extensions to all imports in app.module.ts

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -5,16 +5,16 @@ import {
   RequestMethod,
 } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { PrismaModule } from './prisma/prisma.module';
-import { PostsModule } from './posts/posts.module';
-import { AuthModule } from './auth/auth.module';
-import { JwtMiddleware } from './auth/middleware/jwt.middleware';
-import { RoleMiddleware } from './auth/middleware/role.middleware';
-import { CommantsController } from './comments/comments.controller';
-import { CommantsModule } from './comments/comments.module';
+import { AppController } from './app.controller.js';
+import { AppService } from './app.service.js';
+import { UsersModule } from './users/users.module.js';
+import { PrismaModule } from './prisma/prisma.module.js';
+import { PostsModule } from './posts/posts.module.js';
+import { AuthModule } from './auth/auth.module.js';
+import { JwtMiddleware } from './auth/middleware/jwt.middleware.js';
+import { RoleMiddleware } from './auth/middleware/role.middleware.js';
+import { CommantsController } from './comments/comments.controller.js';
+import { CommantsModule } from './comments/comments.module.js';
 
 @Module({
   imports: [
