12fcfb34d74b30041a3f80f5301fdbd4704b7646
Dev-Abdelrhman <abdelrhmanfawzy38@gmail.com>
2025-12-23 04:57:54 +0200

    Update import paths to use explicit .js extensions in auth module

--- packages/server/src/auth/auth.module.ts
+++ packages/server/src/auth/auth.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
-import { BlackListService } from './blacklist.service';
-import { UsersModule } from 'src/users/users.module';
+import { AuthController } from './auth.controller.js';
+import { AuthService } from './auth.service.js';
+import { BlackListService } from './blacklist.service.js';
+import { UsersModule } from '../users/users.module.js';
 import { PassportModule } from '@nestjs/passport';
-import { UsersService } from 'src/users/users.service';
-import { PrismaModule } from 'src/prisma/prisma.module';
+import { UsersService } from '../users/users.service.js';
+import { PrismaModule } from '../prisma/prisma.module.js';
 
 @Module({
   imports: [
