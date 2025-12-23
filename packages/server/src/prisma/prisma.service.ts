a80c96e067974f73f76f8f74da2cb853b27bf2c1
Dev-Abdelrhman <abdelrhmanfawzy38@gmail.com>
2025-12-23 05:00:01 +0200

    feat: add PostgreSQL adapter and lifecycle management to PrismaService

--- packages/server/src/prisma/prisma.service.ts
+++ packages/server/src/prisma/prisma.service.ts
@@ -1,14 +1,25 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common'
+import { PrismaClient } from '../../prisma/generated/prisma/client.js'
+import { PrismaPg } from '@prisma/adapter-pg'
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super({
+      adapter: new PrismaPg({
+        connectionString: process.env.DATABASE_URL!,
+      }),
+    })
+  }
+
   async onModuleInit() {
-    try {
-      await this.$connect();
-      console.log('Prisma connected to the database');
-    } catch (error) {
-      console.error('Error connecting to the database with Prisma:', error);
-    }
+    await this.$connect()
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect()
   }
 }
